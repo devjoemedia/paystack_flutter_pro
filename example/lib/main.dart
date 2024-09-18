219d1b48cd00a8acf6bb0ea0516e265a2ca96a99
Joseph Nartey <36181621+devjoemedia@users.noreply.github.com>
2024-09-18 08:50:23 +0000

    add: flutter_dotenv

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:paystack_flutter_pro/paystack_flutter_pro.dart'; // Import your package
 
-void main() {
+void main() async{
+    await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,10 +33,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final PaystackFlutterPro _paystackFlutterPro = PaystackFlutterPro('sk_test_658b2e7d7ba05b626f6fdf58892f834fb6829941');
+  final PaystackFlutterPro _paystackFlutterPro =
+      PaystackFlutterPro(dotenv.env['PAYSTACK_SECRET_KEY'].toString());
 
   void _handlePayment(ctx) {
-      _paystackFlutterPro.startTransaction( context: ctx, amount: 10, email: 'customer@mail.com', onSuccess: (){}, onError: (){} );
+    _paystackFlutterPro.startTransaction(
+      context: ctx,
+      amount: 10,
+      email: 'customer@mail.com',
+      onSuccess: () {},
+      onError: () {},
+    );
   }
 
   @override
@@ -47,8 +56,10 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-      TextButton(
-              onPressed: () {_handlePayment(context);},
+            TextButton(
+              onPressed: () {
+                _handlePayment(context);
+              },
               style: TextButton.styleFrom(
                 backgroundColor: Colors.blue,
                 padding: const EdgeInsets.symmetric(
